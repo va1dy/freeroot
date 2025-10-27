74eb9efc344f4152dc7082c5a7863dbd4977dc09
va1dy <fluxuscraft@gmail.com>
2025-10-27 12:30:29 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -3,18 +3,20 @@
 ROOTFS_DIR=$(pwd)
 export PATH=$PATH:~/.local/usr/bin
 max_retries=50
-timeout=1
+timeout=30  # увеличенный таймаут для wget
 ARCH=$(uname -m)
 
+# Определяем архитектуру
 if [ "$ARCH" = "x86_64" ]; then
   ARCH_ALT=amd64
 elif [ "$ARCH" = "aarch64" ]; then
   ARCH_ALT=arm64
 else
-  printf "Unsupported CPU architecture: ${ARCH}"
+  printf "Unsupported CPU architecture: ${ARCH}\n"
   exit 1
 fi
 
+# Установка Ubuntu
 if [ ! -e $ROOTFS_DIR/.installed ]; then
   echo "#######################################################################################"
   echo "#"
@@ -28,8 +30,19 @@ fi
 
 case $install_ubuntu in
   [yY][eE][sS])
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
-      "http://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.3-base-amd64.tar.gz"
+    # Скачиваем Ubuntu только если файл ещё не существует
+    if [ ! -f /tmp/rootfs.tar.gz ]; then
+      echo "Downloading Ubuntu 24.04 for architecture: $ARCH_ALT ..."
+      wget -v --tries=$max_retries --timeout=$timeout -O /tmp/rootfs.tar.gz \
+        "http://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.3-base-${ARCH_ALT}.tar.gz"
+      if [ $? -ne 0 ]; then
+        echo "Failed to download Ubuntu. Exiting."
+        exit 1
+      fi
+    fi
+
+    # Распаковываем
+    echo "Extracting rootfs..."
     tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
     ;;
   *)
@@ -37,35 +50,35 @@ case $install_ubuntu in
     ;;
 esac
 
+# Установка proot
 if [ ! -e $ROOTFS_DIR/.installed ]; then
-  mkdir $ROOTFS_DIR/usr/local/bin -p
-  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
+  mkdir -p $ROOTFS_DIR/usr/local/bin
+  PROOT_URL="https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
 
+  echo "Downloading proot..."
+  wget --tries=$max_retries --timeout=$timeout -O $ROOTFS_DIR/usr/local/bin/proot "$PROOT_URL"
+
+  # Повторяем пока файл пустой
   while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
-    rm $ROOTFS_DIR/usr/local/bin/proot -rf
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
-
-    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-      break
-    fi
-
-    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    rm -f $ROOTFS_DIR/usr/local/bin/proot
+    wget --tries=$max_retries --timeout=$timeout -O $ROOTFS_DIR/usr/local/bin/proot "$PROOT_URL"
     sleep 1
   done
 
   chmod 755 $ROOTFS_DIR/usr/local/bin/proot
 fi
 
+# Настройка resolv.conf
 if [ ! -e $ROOTFS_DIR/.installed ]; then
-  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
-  rm -rf /tmp/rootfs.tar.xz /tmp/sbin
+  mkdir -p $ROOTFS_DIR/etc
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > ${ROOTFS_DIR}/etc/resolv.conf
+  rm -rf /tmp/rootfs.tar.gz /tmp/sbin
   touch $ROOTFS_DIR/.installed
 fi
 
+# Цвета для вывода
 CYAN='\e[0;36m'
 WHITE='\e[0;37m'
-
 RESET_COLOR='\e[0m'
 
 display_gg() {
@@ -74,8 +87,10 @@ display_gg() {
   echo -e "           ${CYAN}-----> Ubuntu 24.04 installed ! <----${RESET_COLOR}"
 }
 
+clear
 display_gg
 
+# Запуск proot
 $ROOTFS_DIR/usr/local/bin/proot \
   --rootfs="${ROOTFS_DIR}" \
   -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
