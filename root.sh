1cc1a51445504e9d532c5bc7f3f2b157432878bc
va1dy <fluxuscraft@gmail.com>
2025-10-27 11:51:43 +0100

    Update root.sh

--- root.sh
+++ root.sh
@@ -34,7 +34,7 @@ case $install_ubuntu in
         if [ ! -f "$ROOTFS_FILE" ]; then
             echo "Downloading Ubuntu rootfs..."
             wget --tries=$MAX_RETRIES --timeout=$TIMEOUT --no-hsts -O "$ROOTFS_FILE" \
-            "http://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.3-base-${ARCH_ALT}.tar.gz" || {
+            "http://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04.3-base-amd64.tar.gz" || {
                 echo "Failed to download rootfs"
                 exit 1
             }
